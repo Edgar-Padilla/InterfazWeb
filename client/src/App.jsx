737ab04a3ac8bc1e002b6469143e597d1425dab0
Edgar <edgarpluis@gmail.com>
2024-09-04 13:43:13 -0600

    crud

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='App'>
+      <div className="datos">
+        <label>Nombre: <input type='text'/></label><br/>
+        <label>Edad: <input type='text'/></label><br/>
+        <label>País: <input type='text'/></label><br/>
+        <label>Cargo: <input type='text'/></label><br/>
+        <label>Años: <input type='text'/></label><br/>
+        <button>Registrar</button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
